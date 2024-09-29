d64e3253abc111c3d52a4d0a4f972f99f585b138
Anna Kipilova <akipilova@gmail.com>
2024-09-29 22:20:19 +0300

    module 2

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import MainPage from '../../pages/main-page.tsx/main-page';
+
+type MainPageProps = {
+    rentalOffers: number;
+  };
+
+function App({rentalOffers}: MainPageProps): JSX.Element {
+    return (
+        <MainPage rentalOffers={rentalOffers} />
+    );
+};
+
+export default App;
